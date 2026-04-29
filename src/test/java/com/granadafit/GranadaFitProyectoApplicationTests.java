3a35985b83f44db49c47f100e644bf20207ccf33
Adrian-Barbos <adrianbarboss17@gmail.com>
2026-04-29 19:36:29 +0200

    Estructura inicial Spring Boot con Maven

---
+++ src/test/java/com/granadafit/GranadaFitProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granadafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranadaFitProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
