cd664669e93e41b28432ee90d65a70929278d917
achraf1kh <achraf.akh12@gmail.com>
2026-05-07 19:07:44 +0200

    He creado todo lo relacionado con la categoria

---
+++ src/main/java/model/Categoria.java
@@ -0,0 +1,20 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria Categoria;
+
+}
