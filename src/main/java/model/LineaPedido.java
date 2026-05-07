40abee8d613ef7b5b795aed1e37b5c33e44669c8
bigote premium <edycallejon@gmail.com>
2026-05-07 19:15:13 +0200

    he creado linea pedidio y pedidio. Y LA INTERFAZ

---
+++ src/main/java/model/LineaPedido.java
@@ -0,0 +1,69 @@
+package model;
+
+
+import com.granadafit.Producto;
+import jakarta.persistence.*;
+
+@Entity
+public class LineaPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer cantidad;
+    private Double precio;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    // Constructor vacío
+    public LineaPedido() {
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
