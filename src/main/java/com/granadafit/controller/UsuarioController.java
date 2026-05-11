801e7fe8acbdfe2463b68016ffabd28eda0f70cd
Adrian-Barbos <adrianbarboss17@gmail.com>
2026-05-11 23:04:18 +0200

    usuario controller

--- src/main/java/com/granadafit/controller/UsuarioController.java
+++ src/main/java/com/granadafit/controller/UsuarioController.java
@@ -1,5 +1,7 @@
-package com.granadafit;
+package com.granadafit.controller;
 
+import com.granadafit.model.Usuario;
+import com.granadafit.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
